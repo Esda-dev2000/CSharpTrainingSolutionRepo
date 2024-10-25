f115aa1d17e6d08f5c043522d1df245239051ae7
Esther Oluwaseye <esther.oluwaseye@principleone.co.uk>
2024-10-25 11:55:58 +0100

    FIRST COMMIT!

---
+++ CSharpClassOneToFourBasics/VaiablesAndConsoleStataements.cs
@@ -0,0 +1,40 @@
+﻿// Declare variables
+string? firstName = string.Empty; // initializing an empty string // the ? is making the variable nullable
+string lastName = string.Empty;
+int age; // initializing to zero does this automatically
+int retirementAge = 65;
+decimal salary;
+char gender = char.MinValue;
+bool isWorking = true;
+
+// prompt the user for input
+Console.WriteLine("Please enter your first name");
+firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name");
+lastName = Console.ReadLine();
+
+// converting from a string to an int
+Console.WriteLine("Please enter your age");
+age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter your salary");
+salary = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Please enter your gender (M or F): ");
+// converting data type to character
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Are you working (true or false): ");
+isWorking = Convert.ToBoolean(Console.ReadLine());
+
+// process the data
+int workingYearsRemaining = retirementAge - age;
+
+// Output the results to the user
+Console.WriteLine($"First Name: {firstName} {lastName}");
+Console.WriteLine($"What is your age: {age}");
+Console.WriteLine($"What is your salary: {salary}");
+Console.WriteLine($"What is your gender: {gender}");
+Console.WriteLine($"Are you employed: {isWorking}");
+Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
---
+++ MathOperatorExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿// assign a variable - to take user input
+int inputNum;
+int numberCheck = 10;
+bool isGreaterOrNotAndEven;
+
+// prompt user for input and convert int
+Console.WriteLine("Please enter your number");
+// Need to convert from string to int when using Console.Readline
+inputNum = Convert.ToInt32(Console.ReadLine());
+
+// process the data - checking if the input number is even and greater than 10
+// To check if a number is even, we use: inputNum % 2 == 0
+// Combine with the check for being greater than 10
+isGreaterOrNotAndEven = (inputNum > numberCheck) && (inputNum % 2 == 0);
+
+// output the results
+Console.WriteLine($"The number is even and greater than 10: {isGreaterOrNotAndEven}");
+Console.WriteLine("My git change");
